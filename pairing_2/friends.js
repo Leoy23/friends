a48745ef76ad7f0594624ea8f7bad97b43370eb3
Leah Young <leah.young21@alumni.colostate.edu>
2022-06-08 20:33:16 -0600

    Add shift method

--- pairing_2/friends.js
+++ pairing_2/friends.js
@@ -3,3 +3,7 @@ var arrays = ["Bob", "Sue", "Tim", "Liam", "Ann"]
 console.log(arrays.slice(2, 3))
 console.log(arrays.pop())
 console.log(arrays.slice(0, 1))
+
+var places = ["Canada", "Sweden", "China", "Bali", "Egypt"]
+
+console.log(arrays.shift())
